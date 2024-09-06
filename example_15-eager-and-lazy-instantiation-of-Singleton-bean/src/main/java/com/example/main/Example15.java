e7b3a218cbf1b7b5ec290fe17faf0605b4f472ca
Ela <micaelafrancisco13@outlook.com>
2024-09-06 15:48:11 +0800

    polish example 15

--- example_15-eager-and-lazy-instantiation-of-Singleton-bean/src/main/java/com/example/main/Example15.java
+++ example_15-eager-and-lazy-instantiation-of-Singleton-bean/src/main/java/com/example/main/Example15.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Example15 {
 
+    /*
+    * With eager instantiation, even before main() gets executed, the Person bean is already created by the Spring IoC container.
+    * In contrast, with lazy instantiation, the Person bean will only be created if and only if it's used (e.g., by getBean(),
+    * dependency injection, or by autowiring).
+    * */
+
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         System.out.println("Before retrieving the Person bean from the Spring Context");
